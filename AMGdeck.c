d01c9c213ffab07ad52e44b2e865e9f909c2de36
Marsel <44898270+Vegacheew@users.noreply.github.com>
2019-07-15 08:57:05 +0200

    no warnings compilation

--- AMGdeck.c
+++ AMGdeck.c
@@ -20,18 +20,13 @@
 #include "param.h"
 #include "log.h"
 
-#include "task.h"
 //#include "stm32f4xx.h"
-
-static uint8_t devAddr;
-static I2C_Dev *I2Cx;
-
-
+//typedef AMG8833_Dev_t amgDev;
 static bool isInit = false;
 
 static float pixelLog[64];
 
-static void amgTask(AMG8833_Dev_t *dev)
+static void amgTask(AMG8833_Dev_t *amgDev)
 {
 	systemWaitStart();
 
@@ -39,38 +34,25 @@ static void amgTask(AMG8833_Dev_t *dev)
     while(1)
     {
     	vTaskDelayUntil(&lastWakeTime, M2T(100));
-    	readPixels(dev, pixelLog, 64);
+    	readPixels(amgDev, pixelLog, 64);
     }
 }
 
-static void amg8833init(AMG8833_Dev_t *dev)
+static void amg8833init()
 {
-	  // Set I2C parameters
-	  I2Cx = I2C1_DEV;
-	  devAddr = AMG88xx_ADDRESS;
-	  bool i2c_complete = i2cdevInit(I2C1_DEV);
-	  // Enter normal mode
-	  bool mode_selected = write8(dev, AMG88xx_PCTL, AMG88xx_NORMAL_MODE);
-	  // Software reset
-	  bool software_resetted = write8(dev, AMG88xx_RST, AMG88xx_INITIAL_RESET);
-	  //disable interrupts by default
-	  bool interrupts_set = disableInterrupt(dev);
-	  //set to 10 FPS
-	  bool fps_set = write8(dev, AMG88xx_FPSC, (AMG88xx_FPS_10 & 0x01));
-	  vTaskDelay(M2T(10));
-	  DEBUG_PRINT("AMG8833 deck driver initialized!\n");
-	  isInit = i2c_complete && mode_selected && software_resetted &&
-	    interrupts_set && fps_set;
-	  readPixels(dev, pixelLog, 64);
-
-	  xTaskCreate(amgTask, AMG_TASK_NAME, AMG_TASK_STACKSIZE, NULL,
-	        MULTIRANGER_TASK_PRI, NULL);
+	DEBUG_PRINT("AMG8833 driver initialized!");
+	isInit = true;
 }
 
 static bool amg8833test()
 {
+	AMG8833_Dev_t *amgDev;
+	amgDev->I2Cx = I2C1_DEV;
+	amgDev->devAddr = AMG88xx_ADDRESS;
 	if (isInit)
 			{
+				begin(amgDev, I2C1_DEV);
+				readPixels(amgDev, pixelLog, 64);
 				DEBUG_PRINT("AMG8833 driver tested!\n");
 			}
 	return true;
@@ -94,6 +76,9 @@ PARAM_ADD(PARAM_UINT8 | PARAM_RONLY, amg8833deck, &isInit)
 PARAM_GROUP_STOP(deck)
 
 LOG_GROUP_START(amg)
-LOG_ADD(LOG_FLOAT, onePixel, &pixelLog[0])
+LOG_ADD(LOG_FLOAT, bottomRight, &pixelLog[0])
+LOG_ADD(LOG_FLOAT, bottomLeft, &pixelLog[7])
+LOG_ADD(LOG_FLOAT, topRight, &pixelLog[55])
+LOG_ADD(LOG_FLOAT, topLeft, &pixelLog[63])
 LOG_ADD(LOG_FLOAT, picture, &pixelLog)
 LOG_GROUP_STOP(amg)
